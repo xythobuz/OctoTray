b6a3f9af437e5246722dfb7e604be92e372839ff
Thomas Buck <xythobuz@xythobuz.de>
2021-05-17 22:08:24 +0200

    linux build without dist folder in zip

--- build_linux.sh
+++ build_linux.sh
@@ -7,10 +7,11 @@ cp src/* build/linux/
 cp data/* build/linux/
 
 cd build/linux
-pyinstaller --noconfirm --onefile --add-data="octotray_icon.png:." octotray.py
+pyinstaller --noconfirm --onefile --name=OctoTray --add-data="octotray_icon.png:." octotray.py
 
-zip -r OctoTray_Linux.zip dist/*
+cd dist
+zip -r OctoTray_Linux.zip *
 
-cd ../..
+cd ../../..
 mkdir -p build/dist
-cp -r build/linux/OctoTray_Linux.zip build/dist
+cp -r build/linux/dist/OctoTray_Linux.zip build/dist
